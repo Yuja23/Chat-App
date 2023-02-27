6f7a25ce6e0ea54986f02d60edf6f2b9482e9dfd
Matthew Rosario <rosariomatt2323@gmail.com>
2023-02-27 21:45:12 +0800

    first commit

---
+++ image/lib/Logics/functions.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Functions {
+  static void updateAvailability() {
+    final firestore = FirebaseFirestore.instance;
+    final auth = FirebaseAuth.instance;
+    final data = {
+      'name': auth.currentUser!.displayName ?? auth.currentUser!.email,
+      'date_time': DateTime.now(),
+      'email': auth.currentUser!.email,
+    };
+    try {
+      firestore.collection('Users').doc(auth.currentUser!.uid).set(data);
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
+}
